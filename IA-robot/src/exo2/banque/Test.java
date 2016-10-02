41294b84a5c066a4003713df8e02853dd0769ee5
victor.audegon <victor.audegon@fa6a82d3-087d-4bbf-a880-887a0b4343df>
2016-10-02 00:08:44 +0000

    test threads

---
+++ IA-robot/src/exo2/banque/Test.java
@@ -0,0 +1,12 @@
+package exo2.banque;
+
+public class Test {
+	  public static void main(String[] args) {
+	    CompteEnBanque cb = new CompteEnBanque();
+
+	    Thread t = new Thread(new RunImpl(cb, "Cysboy"));
+	    Thread t2 = new Thread(new RunImpl(cb, "Zéro"));
+	    t.start();
+	    t2.start();
+	  }
+	} 
